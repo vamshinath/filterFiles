cbf6ea353e4e01c6f5ab095a2776721a27bcf38f
vamshinath <vamshinath.m8@gmail.com>
2019-02-15 13:14:20 +0530

    basic func done

--- mainApp.py
+++ mainApp.py
@@ -1,7 +1,75 @@
 #!/usr/python3
-import sys,os
+import sys,os,shutil
+
+names=[]
+
+def fileOrganizer(allFiles):
+
+    if not os.path.isdir("FILTER"):
+        os.mkdir("FILTER")
+    
+    for dr in allFiles.keys():
+        try:
+            os.mkdir(dr)
+        except Exception as e:
+            k=0
+        ffiles = allFiles.get(dr)
+        for fl in ffiles:
+            try:
+                print(fl)
+                shutil.move(fl,dr+"/"+fl.split("/")[-1])
+            except Exception as e:
+                k=0
+        try:
+            shutil.move(dr,"FILTER/"+dr)
+        except Exception as e:
+            k=0
+    
+
+def scanFiles():
+    files=[]
+    for root,drs,fls in os.walk("."):
+        for fl in fls:
+            try:
+                if os.path.isfile(fl):
+                    fullfl=os.path.abspath(os.path.join(root,fl))
+                    files.append(fullfl)
+            except Exception as e:
+                print(e)
+    return files
+
+def loadNames():
+    global names
+
+    with open("/home/vamshi/names.txt","r") as fl:
+        for ln in fl:
+            names.append(ln[:-1])
+
+def mainEngine(files):
+
+    pqueue={}
+
+    for name in names[::-1]:
+        namefiles=[]
+        for fl in files:
+            exactfl = fl.split("/")[-1]
+            if name in exactfl.lower():
+                namefiles.append(fl)
+                files.remove(fl)
+        pqueue[name]=namefiles
+    
+    fileOrganizer(pqueue)
+
 
 def main():
+
+    os.chdir(sys.argv[1])
+
+    loadNames()
+
+    files = scanFiles()
+
+    mainEngine(files)
     
 
 
